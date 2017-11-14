afb9a3723f77941ff3ac4f580d44204e46e82720
will-Liu-android <will.liu@wolaidai.com>
2017-11-14 17:58:06 +0800

    test

--- src/router.js
+++ src/router.js
@@ -1,8 +1,16 @@
 const routers = [{
-    path: '/',
-    meta: {
-        title: ''
+        path: '/',
+        meta: {
+            title: ''
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve)
     },
-    component: (resolve) => require(['./views/index.vue'], resolve)
-}];
+    {
+        path: '/login',
+        meta: {
+            title: '登录'
+        },
+        component: (resolve) => require(['./views/login/login.vue'], resolve)
+    }
+];
 export default routers;
