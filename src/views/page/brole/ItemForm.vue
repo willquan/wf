dbb4b5f1136ad9fe8cb91cf2d3dc8f3ce9b8f296
willquan <will.liu@wolaidai.com>
2017-12-21 16:21:14 +0800

    增加业务角色功能

---
+++ src/views/page/brole/ItemForm.vue
@@ -0,0 +1,53 @@
+<template>
+<Row type="flex" justify="center" style="margin-bottom: 8px;">
+    <Col span="20" :lg="15">
+        <Card>
+            <p slot="title">业务角色</p>
+            <a href="#" v-if="isEditable" slot="extra" @click.prevent="$refs.wfForm.resetFields()">
+                <Icon type="ios-loop-strong"></Icon>
+                清空表单
+            </a>
+            <Form ref="wfForm" :model="form" :rules="rules" label-position="left" :label-width="100">
+                <FormItem prop="name" label="角色名称">
+                    <Input v-model="form.name" :maxlength="10" placeholder="请输入角色名称" :disabled="!isEditable"  class="check-input"></Input>
+                </FormItem>
+                <FormItem  v-if="isEditable">
+                    <Button @click="handleSubmit" :loading="isLoading" type="primary" long>{{itemId==0 ? '添加' : '修改'}}</Button>
+                </FormItem>
+            </Form>
+        </Card>
+    </Col>
+</Row>
+</template>
+
+<script>
+import formMixin from '@/views/page/mixins/form'
+import {ApiDep, ApiPos, ApiRole} from '@/api/apiUtil'
+import apiMixin from './config'
+
+export default {
+    mixins:[formMixin, apiMixin],
+    name: 'employeeForm',
+    data() {
+        return {
+            form: {
+                name: '',
+            },
+            rules: {
+                name: [
+                    { required: true, message: '角色名称不能为空', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods: {
+        getFormRef() {
+            return this.$refs.wfForm
+        }
+    }
+}
+</script>
+
+<style>
+@import '../common.less';
+</style>
---
+++ src/views/page/brole/page.vue
@@ -0,0 +1,39 @@
+<template>
+<auto-tabs ref="autotabs" :tabs.sync="tabs" :currentTab.sync="currentTab">
+    <table-list v-fix-height ref="list" :slot="tabs[0].name" 
+        @ViewBtnClicked="ViewBtnClicked" 
+        @EditBtnClicked="EditBtnClicked">
+    </table-list>
+    <item-form v-fix-height :slot="add.name" :isEditable="true" @FormDataChanged="DataFormChanged"></item-form>
+    <item-form v-fix-height :slot="edit.name" :isEditable="true" :itemId="editItemId" @FormDataChanged="DataFormChanged"></item-form>
+    <item-form v-fix-height :slot="view.name" :isEditable="false" :itemId="viewItemId"></item-form>
+    <Button type="primary" @click="AddBtnClicked" size="small" slot="extra" style="margin-right: 10px" v-if="hasPermission('create')">{{add.label}}</Button>
+</auto-tabs>
+</template>
+
+<script>
+import AutoTabs from '@/views/components/AutoTabs'
+import ItemForm from './ItemForm'
+import TableList from './TableList'
+import pageMixin from '@/views/page/mixins/page'
+import apiMixin from './config'
+
+export default {
+    mixins:[pageMixin, apiMixin],
+    components: {
+        AutoTabs,
+        ItemForm,
+        TableList
+    },
+    data() {
+        return {
+            add: {name:'AddTab', label: '添加角色', closable: true, show: false},
+            edit: {name:'EditTab', label: '编辑角色', closable: true, show: false},
+            view: {name:'ViewTab', label: '查看信息', closable: true, show: false},
+            tabs: [
+                {name:'ListTab', label: '角色列表', closable: false, show: true}
+            ]
+        }
+    }
+}
+</script>
