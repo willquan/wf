29b0543f5bd2de3a4ab629446334940142776ed5
willquan <1336587782@qq.com>
2017-12-21 21:37:32 +0800

    提交缺陷

--- src/views/page/fault/ItemForm.vue
+++ src/views/page/fault/ItemForm.vue
@@ -3,26 +3,11 @@
     <Col span="20" :lg="20">
         <Card>
             <p slot="title">缺陷单编号：1234123423423</p>
-            <span slot="extra">
-                状态：临时保存
-            </span>
+            <p slot="extra" style="color: green">
+                临时保存
+            </p>
             <Form ref="wfForm" :model="form" :rules="rules" label-position="right" :label-width="100">
-                <Row>
-                    <Col span="12">
-                        <FormItem prop="flevel" label="缺陷级别" v-if="isEditable">
-                            <Select v-model="form.flevel" placeholder="选择缺陷级别" :disabled="!isEditable" transfer>
-                                <Option v-for="el in flevels" :value="el.id" :key="el.id">{{ el.name }}</Option>
-                            </Select>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                        <FormItem prop="fgroup" label="消缺班组" v-if="isEditable">
-                            <Input class="input-disabled-white-bg" v-model="form.fgroup" :maxlength="30" placeholder="请选择消缺班组" :disabled="true">
-                                <Button slot="append" icon="ios-search"></Button>
-                            </Input>
-                        </FormItem>
-                    </Col>
-                </Row>
+                
                 <Row>
                     <Col span="12">
                         <FormItem prop="fpos" label="功能位置">
@@ -39,30 +24,20 @@
                 </Row>
                 <Row>
                     <Col span="12">
-                        <FormItem prop="fpos" label="设备描述">
-                            <Row>
-                                <Input v-model="form.phone" placeholder="自动填充" disabled/>
-                            </Row>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                        <FormItem label="主责班组">
-                            <Input v-model="form.phone" placeholder="自动填充" disabled/>
-                        </FormItem>
-                    </Col>
-                </Row>
-                
-                <FormItem prop="fdesc" label="缺陷描述">
-                    <Input v-model="form.fdesc" :maxlength="200" placeholder="输入缺陷描述"/>
-                </FormItem>
-                <Row>
-                    <Col span="12">
-                        <FormItem prop="ftype" label="缺陷现象类型" v-if="isEditable">
-                            <Select v-model="form.ftype" placeholder="选择缺陷现象类型" :disabled="!isEditable" transfer>
-                                <Option v-for="el in ftypes" :value="el.id" :key="el.id">{{ el.name }}</Option>
+                        <FormItem prop="flevel" label="缺陷级别" v-if="isEditable">
+                            <Select v-model="form.flevel" placeholder="选择缺陷级别" :disabled="!isEditable" transfer>
+                                <Option v-for="el in flevels" :value="el.id" :key="el.id">{{ el.name }}</Option>
                             </Select>
                         </FormItem>
                     </Col>
+                    <Col span="12">
+                        
+                    </Col>
+                </Row>
+                <FormItem prop="fdesc" label="缺陷描述">
+                    <Input v-model="form.fdesc" :maxlength="200" type="textarea" placeholder="输入缺陷描述"/>
+                </FormItem>
+                <Row>
                     <Col span="12">
                          <FormItem prop="finder" label="缺陷发现人" v-if="isEditable">
                             <Input class="input-disabled-white-bg" v-model="form.finder" :maxlength="30" placeholder="请选择缺陷发现人" :disabled="true">
@@ -70,15 +45,13 @@
                             </Input>
                         </FormItem>
                     </Col>
-                </Row>
-                 <Row>
                     <Col span="12">
-                        <FormItem prop="runGrounp" label="运行职别" v-if="isEditable">
-                            <Input class="input-disabled-white-bg" v-model="form.runGrounp" :maxlength="30" placeholder="请选择运行职别" :disabled="true">
-                                <Button slot="append" icon="ios-search"></Button>
-                            </Input>
+                         <FormItem prop="phone" label="联系电话">
+                            <Input v-model="form.phone" :maxlength="30" placeholder="输入联系电话"/>
                         </FormItem>
                     </Col>
+                </Row>
+                 <Row>
                     <Col span="12">
                         <FormItem prop="fixMajor" label="检修专业" v-if="isEditable">
                             <Input class="input-disabled-white-bg" v-model="form.fixMajor" :maxlength="30" placeholder="请选择检修专业" :disabled="true">
@@ -86,16 +59,9 @@
                             </Input>
                         </FormItem>
                     </Col>
-                </Row>
-                <Row>
                     <Col span="12">
-                        <FormItem prop="phone" label="联系电话">
-                            <Input v-model="form.phone" :maxlength="30" placeholder="输入联系电话"/>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                         <FormItem prop="runfg" label="运行分工" v-if="isEditable">
-                            <Input class="input-disabled-white-bg" v-model="form.runfg" :maxlength="30" placeholder="请选择运行分工" :disabled="true">
+                        <FormItem prop="fgroup" label="消缺班组" v-if="isEditable">
+                            <Input class="input-disabled-white-bg" v-model="form.fgroup" :maxlength="30" placeholder="请选择消缺班组" :disabled="true">
                                 <Button slot="append" icon="ios-search"></Button>
                             </Input>
                         </FormItem>
